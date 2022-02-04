49307d9994a5f5940a02fbed1da64e5e4f934162
Amr M. Nouh <AMR.MAMDOUH1371@ci.menofia.edu.eg>
2022-02-04 03:14:07 +0200

    Nader's Presents Problem

---
+++ Challenge/Challenge.cs
@@ -0,0 +1,57 @@
+﻿class Challenge
+{
+    #region SolutionV1.0
+    public static float PresentList(float budget, float bagVolume, int people, int Npresents, float[] presentVolume, float[] presentPrice)
+    {
+        float[,] result = new float[Npresents + 1, (int)Math.Ceiling(bagVolume)];
+        float sol = 0;
+        for (int i = 0; i < result.GetLength(0); i++)
+        {
+            for (int j = 0; j < result.GetLength(1); j++)
+            {
+                if (i == 0 || j == 0) continue;
+                else if (presentVolume[i - 1] <= j)
+                {
+                    sol = presentPrice[i - 1] + result[i - 1, (int)(j - presentVolume[i - 1])];
+                    result[i, j] = Math.Max(sol, result[i - 1, j]);
+                    Console.WriteLine(result[i,j]);
+                }
+                else
+                {
+                    result[i, j] = result[i - 1, j];
+                }
+            }
+
+        }
+
+        for (int i = result.GetLength(1) - 1; i > 0; i--)
+        {
+            int j = result.GetLength(0) - 1;
+            if (result[j, i] <= budget)
+            {
+                sol = result[j, i];
+                break;
+            }
+            else
+            {
+                continue;
+            }
+        }
+
+        return sol;
+    }
+    #endregion
+
+    #region SolutionV2.0
+
+    //private static float SolutionAlgorithm(int numberOfPrestents, float volume,float[] presentVolumes,float[] presnetPrices)
+    //{
+
+    //}
+
+    //public static float PresentList(float budget, float bagVolume, int people, int Npresents, float[] presentVolume, float[] presentPrice)
+    //{
+
+    //}
+    #endregion
+}
---
+++ Challenge/Program.cs
@@ -0,0 +1,5 @@
+﻿float[] volumes = { 4.53f, 9.11f, 4.53f, 6f, 1.04f, 0.87f, 2.57f, 19.45f, 65.59f, 14.14f, 16.66f, 13.53f };
+float[] prices = { 12.23f, 45.03f, 12.23f, 32.93f, 6.99f, 0.46f, 7.34f, 65.98f, 152.13f, 7.23f, 10f, 25.25f };
+Console.WriteLine(Challenge.PresentList(183.23f, 64.11f, 7, 12, volumes, prices));
+
+
